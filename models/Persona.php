bade159fbb3a409993e907980687a5fae73e6bcb
Félix Galindo <galindo.fe@gmail.com>
2019-03-17 08:59:51 -0400

    metodos mágicos get y set

--- models/Persona.php
+++ models/Persona.php
@@ -4,13 +4,27 @@
  */
 class Persona
 {
-  public $id;
-  public $nombre;
+  private $id;
+  private $nombre;
   function __construct($id = null, $nombre = null)
   {
     $this->id = $id;
     $this->nombre = $nombre;
   }
+
+  public function __set($var, $valor) {
+     if (property_exists(__CLASS__, $var)) {
+       $this->$var = $valor;
+     } else {
+       echo "No existe el atributo $var.";
+     }
+   }
+   public function __get($var) {
+     if (property_exists(__CLASS__, $var)) {
+       return $this->$var;
+     }
+     return NULL;
+   }
 }
 
 ?>
